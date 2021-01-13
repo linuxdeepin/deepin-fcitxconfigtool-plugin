9781ab00f5b0fb0cb9f168c372d9210d854a4a08
ut001742 <liuwenhao@uniontech.com>
2021-01-13 14:51:13 +0800

    Feat: 初始化
    Log: 初始化
    
    Change-Id: I5abfd227db4b47993e395a7ac30ab586dd1e26cc

---
+++ src/subconfigwidgettab.h
@@ -0,0 +1,17 @@
+#ifndef SUBCONFIGWIDGETTAB_H
+#define SUBCONFIGWIDGETTAB_H
+
+#include <QLabel>
+
+class subConfigWidgetTab : public QLabel
+{
+    Q_OBJECT
+public:
+    subConfigWidgetTab(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // SUBCONFIGWIDGETTAB_H
