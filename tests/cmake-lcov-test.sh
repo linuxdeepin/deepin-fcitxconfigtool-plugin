2442f604e7995a4c97518ac4c8567eeedd963377
chenshijie <chenshijie@uniontech.com>
2021-02-08 20:21:29 +0800

    feat: 修改单元测试覆盖率统计脚本
    description: 修改单元测试覆盖率统计脚本
    
    Log: 修改单元测试覆盖率统计脚本
    Change-Id: I937f811c471e536e04e2ac47d598b66fe7d8005b

--- tests/cmake-lcov-test.sh
+++ tests/cmake-lcov-test.sh
@@ -5,9 +5,14 @@ cd $workspace
 
 dpkg-buildpackage -b -d -uc -us
 
+make test
+
 project_path=$(cd `dirname $0`; pwd)
+
 #获取工程名
+
 project_name="${project_path##*/}"
+
 echo $project_name
 
 #获取打包生成文件夹路径
@@ -15,7 +20,10 @@ pathname=$(find . -name obj*)
 
 echo $pathname
 
+
 cd $pathname/tests
+
+./deepin-fcitxconfigtool-plugin_test
 
 mkdir -p coverage
 
@@ -26,6 +34,7 @@ lcov --extract ./coverage/coverage.info '*/src/*' -o ./coverage/coverage.info
 lcov --remove ./coverage/coverage.info '*/tests/*' -o ./coverage/coverage.info
 
 mkdir ../report
+
 genhtml -o ../report ./coverage/coverage.info
 
 exit 0
