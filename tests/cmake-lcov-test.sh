4712f36fb550bf231c87cdf908b703b61118814a
chenshijie <chenshijie@uniontech.com>
2021-05-10 15:27:21 +0800

    fix: 修改ut测试相关
    desc: 修改ut测试相关
    
    Log: 修改ut测试相关
    Change-Id: Ifcd8f2e2d60a1e422dd1caac442fb2b006ea214c

--- tests/cmake-lcov-test.sh
+++ tests/cmake-lcov-test.sh
@@ -1,4 +1,42 @@
 #!/bin/bash
+export DISPLAY=:0.0
+utdir=build-ut
+rm -r $utdir
+rm -r ../$utdir
+mkdir ../$utdir
+cd ../$utdir
+
+cmake -DCMAKE_SAFETYTEST_ARG="CMAKE_SAFETYTEST_ARG_ON" .. -DCMAKE_INSTALL_PREFIX="/usr"
+make -j4
+
+touch ./bin/utcase.log
+./bin/deepin-fcitxconfigtool-plugin_test --gtest_output=xml:./report/report.xml | tee ./bin/utcase.log
+
+workdir=$(cd ../$(dirname $0)/$utdir; pwd)
+
+mkdir -p report
+
+lcov -d $workdir -c -o ./report/coverage.info
+
+lcov --extract ./report/coverage.info '*/src/*' -o ./report/coverage.info
+
+lcov --remove ./report/coverage.info '*/tests/*' '*/src/ddeInterface/*' '*/src/fcitxInterface/*' '*/src/widgets/*' '*/src/widgets/labels/*' '*fcitxconfigplugin*' -o ./report/coverage.info
+
+genhtml -o ./report ./report/coverage.info
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
 workspace=$1
 
 cd $workspace
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,19 @@
 #include <QApplication>
 #include <gtest/gtest.h>
 
+#if defined(CMAKE_SAFETYTEST_ARG_ON)
+#include <sanitizer/asan_interface.h>
+#endif
+
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
 
     testing::InitGoogleTest(&argc, argv);
+
+    #if defined(CMAKE_SAFETYTEST_ARG_ON)
+        __sanitizer_set_report_path("asan.log");
+    #endif
 
     return  RUN_ALL_TESTS();
 }
