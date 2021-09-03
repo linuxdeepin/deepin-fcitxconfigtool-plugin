48735fce11fe73d809fa04a91badd7cc68c72b1c
mierke <mierke@uniontech.com>
2021-09-03 14:46:20 +0800

    fix:
    修复输入法管理列表按钮样式问题
    
    Log:
    
    将相关DToolButton控件更换为自定义ClickTabel控件，对原有布局部分参数进行设置，保持界面风格统一
    
    Change-Id: I7dd4291e686bbd08c0dd80f7c6f7854d1992e715

--- src/widgets/labels/clicklabel.cpp
+++ src/widgets/labels/clicklabel.cpp
@@ -3,8 +3,13 @@
 ClickLabel::ClickLabel(QWidget *parent):
     QLabel(parent)
 {
+<<<<<<< HEAD
     setMaximumSize(QSize(16,16));
     setMaximumWidth(25);
+=======
+  //  setMaximumSize(QSize(16,16));
+ //   setMaximumWidth(25);
+>>>>>>> 1144aa4... fix: 修复输入法管理列表按钮样式问题
 }
 
 ClickLabel::~ClickLabel()
