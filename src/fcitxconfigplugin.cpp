bcf5e8e0b0095c56b0c9ae7892a579cf0dce5546
zhaoyue <zhaoyue@uniontech.com>
2022-01-06 15:34:48 +0800

    fix: 修改bug 切换到首位输入法快捷键设置区域，未限制切换输入法快捷键的键入
    添加对shift的判断，添加控制中心新加的纯虚函数
    
    Log:
    Bug: https://pms.uniontech.com/zentao/bug-view-109219.html
    Change-Id: I7ba01379622facb5d940d5447252de983dfee305

--- src/fcitxconfigplugin.cpp
+++ src/fcitxconfigplugin.cpp
@@ -62,6 +62,33 @@ void FcitxConfigPlugin::deactive()
     deleteImWindow();
 }
 
+void FcitxConfigPlugin::addChildPageTrans() const
+{
+    if (m_frameProxy != nullptr) {
+        //defapp
+        m_frameProxy->addChildPageTrans("Input Methods", tr("Input Methods"));
+    }
+}
+
+void FcitxConfigPlugin::initSearchData()
+{
+    QString module = tr("Keyboard and Language");
+    #ifdef QT_DEBUG
+        module = "键盘和语言";
+    #endif
+
+        auto func_process_all = [ = ]() {
+            m_frameProxy->setWidgetVisible(module, tr("Input Methods"), true);
+            m_frameProxy->setDetailVisible(module, tr("Input Methods"), tr("Manage Input Methods"), true);
+            m_frameProxy->setDetailVisible(module, tr("Input Methods"), tr("Switch input methods"), true);
+            m_frameProxy->setDetailVisible(module, tr("Input Methods"), tr("Switch to the first input method"), true);
+            m_frameProxy->setDetailVisible(module, tr("Input Methods"), tr("Advanced Settings"), true);
+            m_frameProxy->updateSearchData(module);
+        };
+
+        func_process_all();
+}
+
 void FcitxConfigPlugin::initialize()
 {
     qDebug() << __FUNCTION__ << QLocale::system().name();
